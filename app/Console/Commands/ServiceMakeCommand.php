ceb45a57bbf2722a67c87ec8d97e258467209342
Eduardo Leite <eduardo.leite@agv.com.br>
2020-11-26 19:34:11 -0300

    ServiceMakeCommand

--- app/Console/Commands/ServiceMakeCommand.php
+++ app/Console/Commands/ServiceMakeCommand.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Console\Commands;
 
 use Illuminate\Console\GeneratorCommand;
@@ -8,49 +7,118 @@ use Symfony\Component\Console\Input\InputArgument;
 class ServiceMakeCommand extends GeneratorCommand
 {
     /**
-     * The name and signature of the console command.
+     * O nome e a assinatura do comando do console.
      *
      * @var string
      */
-    protected $signature = 'make:service';
-
+    protected $name = 'make:service';
+  
     /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Create a new service';
-
+      * A descrição do comando do console.
+      *
+      * @var string
+      */
+    protected $description = 'Create a new service class';
+  
+    /**
+   * O tipo de classe sendo gerada.
+   *
+   * @var string
+   */
+    protected $type = 'Service';
+   
+    /**
+    * Substitui o nome da classe para o stub fornecido.
+    *
+    * @param  string  $stub
+    * @param  string  $name
+    * @return string
+    */
     protected function replaceClass($stub, $name)
     {
         $stub = parent::replaceClass($stub, $name);
-        return str_replace('GenericService', $this->argument('name'), $stub);
+        return str_replace('DummyService', $this->argument('name') . 'Service', $stub);
     }
 
+    /**
+     * Substitui a palavra DummyModel na stub.
+     *
+     * @param  string  $stub
+     * @param  string  $name
+     * @return $this
+     */
+    protected function replaceModel(&$stub, $name)
+    {
+        $class_name = ucfirst($this->argument('name'));
+        $class_name = str_replace('Service', '', $class_name);
+
+        $stub = str_replace('DummyModel', $class_name, $stub);
+
+        return $this;
+    }
+
+    /**
+     * Substitui a palavra dummyModelService na stub.
+     *
+     * @param  string  $stub
+     * @param  string  $name
+     * @return $this
+     */
+    protected function replaceModelService(&$stub, $name)
+    {
+        $class_name = strtolower($this->argument('name'));
+        $class_name = str_replace('service', '', $class_name);
+
+        $stub = str_replace('dummyModel', $class_name, $stub);
+
+        return $this;
+    }
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    protected function buildClass($name)
+    {
+        $stub = $this->files->get($this->getStub());
+
+        return $this->replaceModel($stub, $name)
+            ->replaceModelService($stub, $name)
+        ->replaceClass($stub, $name);
+    }
+
+    /**
+     * Obtpem o arquivo stub para o gerador.
+     *
+     * @return string
+     */
     protected function getStub()
     {
-        return app_path() . 'stubs/service.stub';
+        return  base_path() . '/stubs/service.stub';
     }
-
+    /**
+   * Obtém o namespace padrão para a classe.
+   *
+   * @param  string  $rootNamespace
+   * @return string
+   */
     protected function getDefaultNamespace($rootNamespace)
     {
         return $rootNamespace . '\Services';
     }
 
+    /**
+     * Obtém os argumentos do comando do console.
+     *
+     * @return array
+     */
     protected function getArguments()
     {
         return [
-            ['name', InputArgument::REQUIRED, 'The name of the service']
+            ['name', InputArgument::REQUIRED, 'The name of the service.'],
         ];
-    }
-
-    /**
-     * Execute the console command.
-     *
-     * @return bool|null
-     */
-    public function fire()
-    {
-        return 0;
     }
 }
